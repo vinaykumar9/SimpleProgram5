45bf548197da739fd4784ed39aeacd0f7a405d47
subbareddy9052 <subbufantastic9052@gmail.com>
2020-04-06 17:07:56 +0530

    simple java program

---
+++ src/com/simple/JavaSimple.java
@@ -0,0 +1,19 @@
+package com.simple;
+
+public class JavaSimple {
+
+	public static void main(String[] args) {
+		
+			
+			String s="hungry";
+					if(s=="hungry"){
+						System.out.println("Eat something");
+					}
+					else{
+						System.out.println("Do your own work");
+					}
+				}
+
+			}
+
+	
